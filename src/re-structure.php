afceab93cba118504be94596541df4666ed042ae
Jabran Rafique <hello@jabran.me>
2014-08-04 18:30:50 +0100

    update structure and typo

--- src/re-structure.php
+++ src/re-structure.php
@@ -2,11 +2,15 @@
 
 define('DATAPATH', dirname(dirname(__FILE__)) . '/data');
 
-$file = file_get_contents(DATAPATH . '/islamabad.json');
+$filename = DATAPATH . '/balochistan/barkhan.json';
+
+$file = file_get_contents($filename);
 
 $file = strtolower($file);
 
 $file = str_replace('  ', ' ', $file);
+
+$file = str_replace('-', '_', $file);
 
 $file = preg_replace('/([a-z]+) ([a-z]+)/', '$1_$2', $file);
 
@@ -14,19 +18,98 @@ $file = preg_replace('/([a-z]+) ([a-z]+)/', '$1_$2', $file);
 
 $file = preg_replace('/([0-9]+) (%)/', '$1$2', $file);
 
+$file = str_replace(' (males_per 100 females)', '', $file);
+
+$file = str_replace(' (10 +)', '', $file);
+
+$file = str_replace('sub_divisions', 'tehsil', $file);
+
+$file = str_replace('town_committees', 'cda', $file);
+
+$file = preg_replace('/(rate) \(([0-9]+) [-_] ([0-9]+)\)/', '$1', $file);
+
 $data = json_decode($file);
 
-echo '<pre>';
-print_r($data);
-echo '</pre>';
+// echo '<pre>'; print_r($data); echo '</pre>'; return;
 
 $o = array();
 
-$o['title'] = ucfirst($data->title);
-$o['area'] = '';
-$o['population'] = '';
-$o['administration'] = '';
-$o['litracy_ratio'] = '';
-$o['growth_rate'] = '';
-$o['household'] = '';
-$o['housing'] = '';
+$title = substr($data->title, 0, strpos($data->title, '_at_'));
+$title = implode(' ', explode('_', $title));
+
+$o['title'] = ucwords($title);
+
+$o['area']['value'] = floatval($data->area);
+$o['area']['unit_long'] = 'square kilometers';
+$o['area']['unit_short'] = 'Sq. Kms';
+
+$population = (array) $data->population;
+
+$o['population']['year'][array_keys($population)[0]] = intval(array_values($population)[0]);
+
+$o['population']['gender']['male']['percentage'] = floatval($data->male);
+$o['population']['gender']['female']['percentage'] = floatval($data->female);
+
+$o['population']['ratio']['type'] = 'males per 100 females';
+$o['population']['ratio']['value'] = floatval($data->sex_ratio);
+
+$o['population']['density']['value'] = floatval($data->population_density);
+$o['population']['density']['unit_long'] = 'per square kilometers';
+$o['population']['density']['unit_short'] = 'per Sq. Kms';
+
+$urban_pop = preg_match('/[0-9]+/', $data->urban_population, $urban_pop_match);
+$urban_per = preg_match('/\(([0-9\.]+)%\)/', $data->urban_population, $urban_per_match);
+
+$o['population']['urban']['percentage'] = floatval($urban_per_match[1]);
+$o['population']['urban']['value'] = intval($urban_pop_match[0]);
+
+$rural_pop = preg_match('/[0-9]+/', $data->rural_population, $rural_pop_match);
+$rural_per = preg_match('/\(([0-9\.]+)%\)/', $data->rural_population, $rural_per_match);
+
+$o['population']['rural']['percentage'] = floatval($rural_per_match[1]);
+$o['population']['rural']['value'] = intval($rural_pop_match[0]);
+
+$o['administration']['tehsils'] = intval($data->tehsil);
+$o['administration']['union_councils'] = intval($data->cda);
+$o['administration']['mauzas'] = intval($data->mauzas);
+
+$o['litracy_ratio']['age'] = '10+';
+$o['litracy_ratio']['percentage'] = floatval($data->literacy_ratio);
+
+$o['growth_rate']['type'] = 'average';
+$o['growth_rate']['percentage'] = floatval($data->average_annual_growth_rate);
+$o['growth_rate']['year']['from'] = 1981;
+$o['growth_rate']['year']['to'] = 1998;
+
+$o['household']['type'] = 'average';
+$o['household']['size'] = floatval($data->average_household_size);
+
+$o['housing']['units']['total'] = intval($data->total_housing_units);
+
+$pacca_val = preg_match('/([0-9]+) \(/', $data->pacca_housing_units, $pacca_val_match);
+$pacca_per = preg_match('/\(([0-9\.]+)%\)/', $data->pacca_housing_units, $pacca_per_match);
+
+$o['housing']['units']['pacca']['value'] = intval($pacca_val_match[1]);
+$o['housing']['units']['pacca']['percentage'] = floatval($pacca_per_match[1]);
+
+$electric_val = preg_match('/([0-9]+) \(/', $data->housing_units_having_electricity, $electric_val_match);
+$electric_per = preg_match('/\(([0-9\.]+)%\)/', $data->housing_units_having_electricity, $electric_per_match);
+
+$o['housing']['units']['have_utilities']['electricity']['value'] = intval($electric_val_match[1]);
+$o['housing']['units']['have_utilities']['electricity']['percentage'] = floatval($electric_per_match[1]);
+
+$water_val = preg_match('/([0-9]+) \(/', $data->housing_units_having_piped_water, $water_val_match);
+$water_per = preg_match('/\(([0-9\.]+)%\)/', $data->housing_units_having_piped_water, $water_per_match);
+
+$o['housing']['units']['have_utilities']['water']['type'] = 'piped';
+$o['housing']['units']['have_utilities']['water']['value'] = intval($water_val_match[1]);
+$o['housing']['units']['have_utilities']['water']['percentage'] = floatval($water_per_match[1]);
+
+$gas_val = preg_match('/([0-9]+) \(/', $data->housing_units_using_gas_for_cooking, $gas_val_match);
+$gas_per = preg_match('/\(([0-9\.]+)%\)/', $data->housing_units_using_gas_for_cooking, $gas_per_match);
+
+$o['housing']['units']['have_utilities']['gas']['value'] = intval($gas_val_match[1]);
+$o['housing']['units']['have_utilities']['gas']['percentage'] = floatval($gas_per_match[1]);
+
+header('Content-Type: application/json');
+echo json_encode($o, JSON_PRETTY_PRINT);
