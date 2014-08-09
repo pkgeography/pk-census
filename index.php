3f40d43ffe9ecb5a60cefb556f24c6cf1880e2f0
Jabran Rafique <hello@jabran.me>
2014-08-09 06:58:10 +0100

    update file permissions

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DIRPATH', __DIR__ . '/data');
+
+$files = array();
+
+$dirs = array(
+	DIRPATH,
+	DIRPATH . '/balochistan',
+	DIRPATH . '/nwfp',
+	DIRPATH . '/punjab',
+	DIRPATH . '/sindh'
+);
+
+// foreach
